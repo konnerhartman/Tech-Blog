25ab77fe1451daa16c3c596224d8c6213c9cbd9e
Konner Hartman <konner.hartman@yahoo.com>
2021-11-11 18:10:41 -0600

    Added code to models folder.

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_comment: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            refrences: {
+                model: 'user',
+                key: 'key'
+            }
+        },
+       post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            refrences: {
+                model: 'post',
+                key: 'key'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
