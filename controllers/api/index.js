c133654cd7a08a468b37f1e26aa75b5337bf0233
Konner Hartman <konner.hartman@yahoo.com>
2021-11-20 16:13:11 -0600

    added handlebars code to all files. Still buggy though.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,4 +7,8 @@ router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
+router.use((req, res) => {
+    res.send("<h1>Wrong Route!</h1>")
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const homeRoutes = require('/homeRoutes');
-const apiRoutes = require('/api');
-const dashboardRoutes = require('/dashboardRoutes');
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.send("<h1>Wrong Route!</h1>")
+});
 
 module.exports = router;
