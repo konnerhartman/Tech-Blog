e39c9012035dfece0adcedea9bd99f222bb6cc9e
Konner Hartman <konner.hartman@yahoo.com>
2021-11-20 18:35:12 -0600

    sign up feature is working now.

--- public/js/login.js
+++ public/js/login.js
@@ -22,31 +22,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#usernameSignup').value.trim();
-  const password = document.querySelector('#passwordSignup').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document
   .querySelector('.loginForm')
   .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signupForm')
-  .addEventListener('submit', signupFormHandler);
