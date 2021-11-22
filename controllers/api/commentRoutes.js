67bf771a09971d16494ec1bae36ecf655b0ca26d
Konner Hartman <konner.hartman@yahoo.com>
2021-11-22 12:34:50 -0600

    added comment route code

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,53 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+// The `/api/comments` endpoint
+// get all comments
+router.get('/', async (req, res) => {
+  // find all comments
+  // be sure to include its associated Category and Tag data
+  try {
+    const commentData = await Comment.findAll({});
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new comment
+router.post('/', withAuth, (req, res) => {
+  Comment.create({
+    user_comment: req.body.user_comment,
+    post_id: req.body.post_id,
+    user_id: req.session.user_id
+  }).then((result)=>{
+    res.status(200).json(result)
+  })
+  
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete one comment by its `id` value
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -8,24 +8,11 @@ const commentData = require('./commentData.json')
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  await User.bulkCreate(userData);
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-  
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  await Post.bulkCreate(postData);
+
+  await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
